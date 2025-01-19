4ba01e7d9a72d8bbae2a5f01e0be8c2036c59478
Vihanga Mallawaarachchi <vihanganethusara00@gmail.com>
2025-01-19 11:23:32 +0530

    setup: prettier and tailwind plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,45 @@
-import type { Config } from "tailwindcss";
+/* eslint-disable @typescript-eslint/no-require-imports */
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        poppins: ['var(--font-poppins)', 'sans-serif'],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#333',
+            h1: { color: '#1a202c' },
+            a: { color: '#3182ce', textDecoration: 'none' },
+          },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('tailwindcss-animate'),
+  ],
 } satisfies Config;
